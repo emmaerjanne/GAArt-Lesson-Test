dc4b499c837af177809e28b8dec76861294a985f
Emma Erjanne <e.erjanne@gmail.com>
2021-10-28 21:23:32 +0300

    Added a particle system

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //speed we want the char to move
+    public float speed = 1;
+    //how powerful char can jump
+    public float jumpForce = 1;
+    //distance to check from the char to the ground
+    public float groundCheckDistance = 0.5f;
+    //the sprite
+    SpriteRenderer sprite = null;
+    //rigidbody ref for the physics
+    RigidBody2D rb = null;
+    //check if the character is in the air
+    bool inAir = false;
+    //direction
+    float scaleX = 1;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<RigidBody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        scaleX = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.right * Input.GetAxis("Horizontal") * speed + Vector2.up * rb.velocity.y;
+
+        if(Input.GetAxis("Horizontal") != 0) {
+            int direction = 1;
+            if (Input.GetAxis("Horizontal") < 0) {
+                direction = -1;
+            }
+            transform.localScale = new Vector3(scaleX * direction, transform.localScale.y, transform.localScale.z);
+            
+        }
+        //make sure the char is on the ground by checking the distance from the char downwards
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance);
+        //if there is ground close enough, you aren't in the air
+        inAir = hit.collider == null;
+
+        //if the char is on the ground and "jump" spacebar is pushed, the char jumps
+        if(!inAir && Input.GetButtonDown("Jump")) {
+            transform.position += Vector3.up * 0.1f;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
